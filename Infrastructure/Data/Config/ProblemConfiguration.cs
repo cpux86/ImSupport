f20ef0a4ac6f6332ccddb336740b8dbdadd7cb9c
Владимир <cpux86@mail.ru>
2022-05-30 16:37:02 +0300

    Backup

---
+++ Infrastructure/Data/Config/ProblemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AppCore.Modeles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class ProblemConfiguration : IEntityTypeConfiguration<Problem>
+    {
+        public void Configure(EntityTypeBuilder<Problem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Data/Context/ProblemContext.cs
@@ -0,0 +1,42 @@
+﻿using AppCore.Modeles;
+using Data.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class ProblemContex : DbContext
+    {
+        public ProblemContex(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Problem>? Problems { get; set; }
+        public DbSet<Device>? Devices { get; set; }
+        public DbSet<Location>? Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                .UseSqlite(@"DataSource=C:\C#\ImSupport\DB\ImSupport.db");
+                //optionsBuilder
+                //    .UseNpgsql("Host=localhost;Port=5432;Database=zion;Username=cpux86;Password=1AC290066F");
+            }
+
+            optionsBuilder.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+            Console.Clear();
+            optionsBuilder.LogTo(message => Console.WriteLine(message), Microsoft.Extensions.Logging.LogLevel.Information);
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProblemConfiguration());
+        }
+    }
+}
