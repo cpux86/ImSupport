ffa2ea32922ffc1756d16c57d464308e580f72ff
Владимир <cpux86@mail.ru>
2022-06-09 19:11:44 +0300

    backup

--- App/AppCore/Models/Location.cs
+++ App/AppCore/Models/Location.cs
@@ -8,7 +8,7 @@ namespace AppCore.Models
 {
     public class Location : BaseEntity
     {
-        public int Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public List<Device> Devices { get; set; } = new List<Device>();
     }
 }
---
+++ Data/Migrations/20220609060701_Init2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class Init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Locations",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
