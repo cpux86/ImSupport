b49c108a3eff434c64024bfb9306cc90ec372b4f
Владимир <cpux86@mail.ru>
2022-07-19 16:58:15 +0300

    Add MediatR

---
+++ src/Application/Features/Appeals/Commands/CreateAppeal/CreateAppealCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Features.Appeals.Commands.CreateAppeal
+{
+    public class CreateAppealCommandHandler : IRequestHandler<CreateAppealCommand>
+    {
+        public Task<Unit> Handle(CreateAppealCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
